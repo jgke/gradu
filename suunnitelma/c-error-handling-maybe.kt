b59838451d42f57654e427ab731622a5122a542b
Jaakko Hannikainen <jaakko.hannikainen@solita.fi>
2018-10-09 16:24:52 +0300

    Progress

--- suunnitelma/c-error-handling-maybe.kt
+++ suunnitelma/c-error-handling-maybe.kt
@@ -1,26 +1,26 @@
-dev_priv->perf.metrics_kobj !> {
+dev_priv->perf.metrics_kobj ?: {
     DRM_DEBUG("OA metrics weren't advertised via sysfs\n")
-    -EINVAL
+    return -EINVAL
 }
 
-props->sample_flags & SAMPLE_OA_REPORT !> {
+props->sample_flags & SAMPLE_OA_REPORT ?: {
     DRM_DEBUG("Only OA report sampling supported\n")
-    -EINVAL
+    return -EINVAL
 }
 
-dev_priv->perf.oa.ops.init_oa_buffer !> {
+dev_priv->perf.oa.ops.init_oa_buffer ?: {
     DRM_DEBUG("OA unit not supported\n")
-    -ENODEV
+    return -ENODEV
 }
 
 /* ... */
 
-stream->ctx ?> oa_get_render_ctx_id(stream) ?> {
+stream->ctx?.let { oa_get_render_ctx_id(stream) } ?: {
     DRM_DEBUG("Invalid context id to filter with\n")
     return it
 }
 
-get_oa_config(dev_priv, props->metrics_set, &stream->oa_config) ?> {
+get_oa_config(dev_priv, props->metrics_set, &stream->oa_config)?.let {
     ret = it
     DRM_DEBUG("Invalid OA config id=%i\n", props->metrics_set)
     goto err_config
@@ -29,7 +29,7 @@ get_oa_config(dev_priv, props->metrics_set, &stream->oa_config) ?> {
 /* ... */
 
 err_config:
-stream->ctx ?> oa_put_render_ctx_id(stream)
+stream->ctx?.let { oa_put_render_ctx_id(stream) }
 
 return ret
 
