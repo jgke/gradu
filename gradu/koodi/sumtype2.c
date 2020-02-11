aebd359eaa88ea85822ee7a09a5eb71496ae1c85
Jaakko Hannikainen <jaakko.hannikainen@solita.fi>
2020-02-11 16:44:15 +0200

    Progress

--- gradu/koodi/sumtype2.c
+++ gradu/koodi/sumtype2.c
@@ -1,16 +1,7 @@
 struct Puu;
 
-struct _Puu_Lehti {
-    struct Puu *_d;
-    int _1;
-};
+struct _Puu_Lehti { struct Puu *_d; int _1; };
 
-struct _Puu_Haara {
-    struct Puu *_1;
-    struct Puu *_2;
-};
+struct _Puu_Haara { struct Puu *_1; struct Puu *_2; };
 
-union Puu {
-    struct _Puu_Lehti Lehti;
-    struct _Puu_Haara Haara;
-};
+union Puu { struct _Puu_Lehti Lehti; struct _Puu_Haara Haara; };
